6558d1d83b0a0be5ef0520e26530cd3a423dac09
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2018-09-29 16:19:32 +0300

    first

---
+++ src/com/app/Main.java
@@ -0,0 +1,28 @@
+package com.app;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final int[] nums = {
+                12, 4, 12, 12, 7, 7, 7
+        };
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int numOfOnes = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (((nums[j] >> i) & 1) == 1) {
+                    numOfOnes++;
+                    numOfOnes %= 3;
+                }
+            }
+            if (numOfOnes == 1) {
+                res |= (numOfOnes << i);
+            }
+        }
+        return res;
+    }
+}
